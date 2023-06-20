ba9958620d00b67ef082b3f572cf8cd9893cef83
Amish Pratap Singh <amishpratapsingh13@gmail.com>
2023-06-20 16:34:21 +0530

    completed the dropdown for active search

--- Movie war/index.js
+++ Movie war/index.js
@@ -11,17 +11,43 @@ const fetchData = async (searchTerm) => {
     return response.data.Search;
 };
 
+const root = document.querySelector('.autoComplete');
+root.innerHTML=`
+    <label><b>Search For a Movie</b></label>
+    <input class="input"/>
+    <div class="dropdown">
+        <div class="dropdown-menu">
+            <div class = "dropdown-content results"></div>
+        </div>
+    </div>
+`;
 const input = document.querySelector('input');
+const dropdown = document.querySelector('.dropdown');
+const resutlsWrapper = document.querySelector('.results');
+
 
 const onInput = async event => {
     const movies = await fetchData(event.target.value);
+    if(!movies.length){
+        dropdown.classList.remove('is-active');
+        return;
+    }
+    resutlsWrapper.innerHTML="";
+    dropdown.classList.add('is-active');
     for(let movie of movies){
-        const div = document.createElement('div');
-        div.innerHTML=`
-        <img src="${movie.Poster}" />
-        <h1>${movie.Title}</h1>
-        `;
-        document.querySelector('#target').appendChild(div);
+        const option = document.createElement('a');
+        const imgSrc=movie.Poster==='N/A'?'':movie.Poster;
+        option.classList.add('dropdown-item');
+        option.innerHTML=`
+        <img src="${imgSrc}" />
+        ${movie.Title}
+        `; 
+        resutlsWrapper.appendChild(option);
     }
 };
-input.addEventListener('input', debounce(onInput));
+input.addEventListener('input', debounce(onInput,500));
+document.addEventListener('click',event=>{
+    if(!root.contains(event.target)){
+        dropdown.classList.remove('is-active');
+    }
+})
