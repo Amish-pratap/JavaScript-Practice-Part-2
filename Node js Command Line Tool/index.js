c4222b46057ea5384fce2fbdc19da732a9c8a152
Amish Pratap Singh <amishpratapsingh13@gmail.com>
2023-06-25 14:49:58 +0530

    created the package jason file with bin section

--- Node js Command Line Tool/index.js
+++ Node js Command Line Tool/index.js
@@ -1,1 +1,13 @@
-cons
+const fs = require('fs');
+//file system module in node
+
+fs.readdir(process.cwd(),(err,filenames)=>{
+    //either
+    //err===an error , which means something went ron 
+    //or
+    // err==null , which mean everything is ok
+    if(err){
+        console.log(err);
+    }
+    console.log(filenames);
+});
